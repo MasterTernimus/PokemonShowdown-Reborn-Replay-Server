e18c583759b290a56990f1e6484d30dc9443a29b
Master Ternimus <chinmaya.kallaje7@gmail.com>
2026-02-27 18:48:07 +0530

    Update generate_replays.py

--- generate_replays.py
+++ generate_replays.py
@@ -3,7 +3,26 @@ import os
 import re
 from collections import defaultdict
 from datetime import datetime
+import math
 
+def hide_full_damage(log: list):
+    for i, line in enumerate(log):
+        if any(word in line for word in ("|damage|", "|-damage|", "|-heal|", "|switch|")):
+            damage = line.split("|")[4] if "|switch|" in line else line.split("|")[3]
+            if damage == "0 fnt":
+                continue
+            damage = damage.split(" ")[0]
+
+            hp = math.ceil(int(damage.split("/")[0]) / int(damage.split("/")[1]) * 100)
+            line = line.replace(damage, f"{hp}/100")
+            log[i] = line
+
+    return log
+
+def get_unix_timestamp(timestamp: str):
+    timestamp = " ".join(timestamp.split(" ")[:5])
+    timestamp = datetime.strptime(timestamp, "%a %b %d %Y %H:%M:%S")
+    return int(timestamp.timestamp())
 
 def create_replay_object(log: dict, show_full_damage: bool = False):
     if not all(key in log for key in ('p1', 'p2', 'log', 'inputLog', 'roomid', 'format')):
